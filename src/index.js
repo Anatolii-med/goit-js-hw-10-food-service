763b8fca04897816ca54677f6fcd56e57d8d0a15
Anatolii-med <anatolii@github.com>
2021-06-28 22:30:19 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import menuTemplate from './templates/menu_items.hbs';
+import menuList from './menu.json';
+
 import './sass/main.scss';
+
+console.log(menuTemplate(menuList));
+
+const menu = document.querySelector('.js-menu');
+
+menu.innerHTML = menuTemplate(menuList);
