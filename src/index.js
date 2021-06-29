7166e8f752295c2c1490fe559dc707a75d4efd92
Anatolii-med <67787169+Anatolii-med@users.noreply.github.com>
2021-06-29 17:54:25 +0300

    111

--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import menuList from './menu.json';
 
 import './sass/main.scss';
 
-console.log(menuTemplate(menuList));
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 const menu = document.querySelector('.js-menu');
-
 menu.innerHTML = menuTemplate(menuList);
+
+const themeSwitcher = document.querySelector('#theme-switch-toggle');
+const bodyTheme = document.querySelector('body');
+
+// localStorage.getItem('themeMod');
+// if (themeMod === dark) {
+//   bodyTheme.classList.add(Theme.DARK);
+//   bodyTheme.classList.remove(Theme.LIGHT);
+// }
+
+bodyTheme.classList.add(Theme.LIGHT);
+// localStorage.setItem('themeMod', 'light');
+
+const themeChange = function () {
+  if (bodyTheme.classList.contains(Theme.LIGHT)) {
+    localStorage.setItem('themeMod', 'dark');
+    bodyTheme.classList.add(Theme.DARK);
+    bodyTheme.classList.remove(Theme.LIGHT);
+  } else {
+    localStorage.setItem('themeMod', 'light');
+    bodyTheme.classList.add(Theme.LIGHT);
+    bodyTheme.classList.remove(Theme.DARK);
+  }
+};
+
+themeSwitcher.addEventListener('change', themeChange);
