60ae600a1b54cc50c898c5d2f824af640cf558c1
Anatolii-med <anatolii@github.com>
2021-06-29 23:29:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -12,27 +12,43 @@ const menu = document.querySelector('.js-menu');
 menu.innerHTML = menuTemplate(menuList);
 
 const themeSwitcher = document.querySelector('#theme-switch-toggle');
+themeSwitcher.addEventListener('change', themeChange);
+
 const bodyTheme = document.querySelector('body');
 
-// localStorage.getItem('themeMod');
-// if (themeMod === dark) {
-//   bodyTheme.classList.add(Theme.DARK);
-//   bodyTheme.classList.remove(Theme.LIGHT);
-// }
-
-bodyTheme.classList.add(Theme.LIGHT);
-// localStorage.setItem('themeMod', 'light');
-
-const themeChange = function () {
-  if (bodyTheme.classList.contains(Theme.LIGHT)) {
-    localStorage.setItem('themeMod', 'dark');
-    bodyTheme.classList.add(Theme.DARK);
-    bodyTheme.classList.remove(Theme.LIGHT);
-  } else {
-    localStorage.setItem('themeMod', 'light');
-    bodyTheme.classList.add(Theme.LIGHT);
-    bodyTheme.classList.remove(Theme.DARK);
+function themeChange(evt) {
+  const checked = evt.currentTarget.checked;
+  if (checked) {
+    setDarkTheme();
   }
-};
 
-themeSwitcher.addEventListener('change', themeChange);
+  if (!checked) {
+    setLightTheme();
+  }
+}
+
+checkCurrentTheme();
+
+function checkCurrentTheme() {
+  const currentTheme = localStorage.getItem('themeMod');
+  if (currentTheme === Theme.LIGHT || currentTheme === null) {
+    setLightTheme();
+  }
+
+  if (currentTheme === Theme.DARK) {
+    setDarkTheme();
+  }
+}
+
+function setDarkTheme() {
+  localStorage.setItem('themeMod', Theme.DARK);
+  bodyTheme.classList.add(Theme.DARK);
+  bodyTheme.classList.remove(Theme.LIGHT);
+  themeSwitcher.checked = true;
+}
+function setLightTheme() {
+  localStorage.setItem('themeMod', Theme.LIGHT);
+  bodyTheme.classList.add(Theme.LIGHT);
+  bodyTheme.classList.remove(Theme.DARK);
+  themeSwitcher.checked = false;
+}
